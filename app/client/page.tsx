ab0a5b190a4ad3d6ebac62e892de58a1dc21bcd2
Hasan <hassannawaz1420@gmail.com>
2026-05-26 10:14:22 +0500

    add ClientPage component and update Home function to fetch jokes

---
+++ app/client/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from 'react'
+
+const ClientPage = () => {
+  return (
+    <div>
+        <button onClick={()=> alert("hellow je")}>
+            click me
+        </button>
+    </div>
+  )
+}
+
+export default ClientPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+
+  const res = await fetch("https://api.freeapi.app/api/v1/public/randomjokes/100")
+  const data = await res.json()
+  console.log(data)
+
+
+
   return (
     <h1 className="text-3xl font-bold">Home Page</h1>
   );
